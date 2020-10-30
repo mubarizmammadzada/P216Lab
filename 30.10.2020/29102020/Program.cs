0073633fe098ae21453b55dd55b114634b8b7d97
mubucimbom <mubarizshm@code.edu.az>
2020-10-30 22:35:57 +0400

    LabTasks

---
+++ 30.10.2020/29102020/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29102020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task1
+            //Daxil edilmish ededin faktorialini hesablayin
+            //Console.WriteLine("Eded daxil edin");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int result = 1;
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    result *= i;
+            //}
+            //Console.WriteLine($"Daxil etdiyiniz ededin faktoriali:{result}");
+            //Task2
+            //verilmish ededin neche hedli oldugunu tapin 
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int lastDigit = 0;
+            //int counter = 0;
+            //while (number>0)
+            //{
+            //    lastDigit = number % 10;
+            //    number = (number - lastDigit) / 10;
+            //    counter++;
+
+            //}
+            //Console.WriteLine(counter);
+            //Task3
+            //verilmish ededin en boyuk reqemini tapin 
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int lastDigit = 0;
+            //int greatDigit = 0;
+            //while (number>0)
+            //{
+            //    lastDigit = number % 10;
+            //    number = (number - lastDigit) / 10;
+            //    if (lastDigit > greatDigit)
+            //    {
+            //        greatDigit = lastDigit;
+            //    }
+            //}
+            //Console.WriteLine($"Ededin en boyuk heddi:{greatDigit}");
+            //Task4
+            //Iki eded daxil edilir.Number ededine qeder butun edednlerin usdu power ededlerinin cemini hesablayin
+            //Console.WriteLine("Ededi Daxil edin");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Quvveti daxil edin");
+            //int power = Convert.ToInt32(Console.ReadLine());
+            //int multiply = 1;
+            //int sum = 0;
+
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    for (int j = 1; j <= power; j++)
+            //    {
+            //        multiply *= i;
+            //    }
+            //    sum += multiply;
+            //    multiply = 1;
+
+            //}
+            //Console.WriteLine($"Netice:{sum}");
+
+            //Task5
+            //Studen classi yaradin.Classin ad,soyad,imtahan neticesi propertyleri olsun.
+            //Imtahan methodu yazin eger imtahan neticesi 50-den boyukdurse imtahandan kechdiniz,deyilse imtahandan kesildiniz cixarin ekrana.
+            //Student student = new Student();
+            //Console.Write("Adi daxil edin:");
+            //student.Name = Console.ReadLine();
+            //Console.Write("Soyadinizi Daxil Edin:");
+            //student.Surname = Console.ReadLine();
+            //Console.Write("Imtahan Neticenizi Daxil Edin:");
+            //student.ExamResult = Convert.ToInt32(Console.ReadLine());
+            //student.Exam();
+        }
+
+
+    }
+}
---
+++ 30.10.2020/29102020/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29102020
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int ExamResult { get; set; }
+
+        public void Exam()
+        {
+            if (ExamResult > 50)
+            {
+                Console.WriteLine("Imtahandan kecdiniz.Diplomu gelin goturun)))");
+            }
+            else
+            {
+                Console.WriteLine("Imtahannan kesildiniz.Narmalni oxuyardiz");
+            }
+        }
+    }
+}
